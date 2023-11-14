f3d5dec7e7c554f2a8c450fa768694f5f64a8af1
MMostafa-Hub <mmostafa.ibra21@gmail.com>
2023-11-14 23:43:58 +0200

    Add support for avoiding double key presses in KeyboardAnnotator

--- annotator/keyboard_annotator.py
+++ annotator/keyboard_annotator.py
@@ -1,13 +1,17 @@
-from typing import Dict
+from pynput.keyboard._win32 import KeyCode
+from pynput.keyboard import Key, Listener
 from .annotator import Annotator
 from datetime import datetime
-from pynput.keyboard import Key, Listener
+from typing import Dict
 
 
 class KeyboardAnnotator(Annotator):
     def __init__(self, config: Dict[str, str]):
         super().__init__(config)
         self.timer: datetime = 0
+
+        # The last pressed key to avoid double presses
+        self.last_pressed_key = None
 
         # Create a dictionary that maps the keyboard keys to the description
         self.keyboard_mapping: dict = {}
@@ -16,15 +20,39 @@ class KeyboardAnnotator(Annotator):
                 self.keyboard_mapping[key.lower()] = value
             else:
                 self.keyboard_mapping[getattr(Key, key.lower())] = value
-                
+        print(self.keyboard_mapping)
         # Create a listener for the keyboard
         with Listener(on_press=self.on_press, on_release=self.on_release) as listener:
             listener.join()
 
-    def on_press(self, key):
+    def on_press(self, key: KeyCode):
+        key = key.char if hasattr(key, "char") else key
+        if key not in self.keyboard_mapping:
+            return
+
+        # Check if the key is the same as the last pressed key
+        if key == self.last_pressed_key:
+            return
+
+        self.last_pressed_key = key
+
         self.timer = datetime.now()
         self.onset.append(self.timer.second)
-        self.description.append(self.keyboard_mapping[key])
+        self.description.append(self.keyboard_mapping.get(key))
+        print(self.keyboard_mapping.get(key))
 
     def on_release(self, key):
-        self.duration.append(datetime.now().second - self.timer.second)
+        # stop the listener
+        if key == Key.esc:
+            return False
+
+        # Check if the key is in the mapping
+        key = key.char if hasattr(key, "char") else key
+        if key not in self.keyboard_mapping:
+            return
+
+        # set the duration of the pressed key
+        self.duration.append((datetime.now() - self.timer).seconds)
+
+        # reset the last pressed key
+        self.last_pressed_key = None
